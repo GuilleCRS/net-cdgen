d8f78413a22bd0d0a9086fb3000ee0f4cd13b4ce
Guillermo Cruz <guillermocrsa@gmail.com>
2020-10-19 15:15:09 -0600

    Create controller refactor

--- ControllerHandler/CreateController.cs
+++ ControllerHandler/CreateController.cs
@@ -7,62 +7,121 @@ namespace net_cdgen.ControllerHandler
     public class CreateController
     {
         private string[] args;
+
         public CreateController(string[] args)
         {
             this.args = args;
-            if (args[1] == "-m" || args[1] == "--model")
+            if (args.Length == 5)
             {
-                string model = args[2];
-                Console.WriteLine($"Creating {model} controller!");
-                if (args[3] == "-nm" || args[3] == "--namespace")
+                if (args[1] == "-m" || args[1] == "--model")
                 {
-                    string nmspace = args[4];
-                    Console.WriteLine(nmspace);
-                    string controllersDirPath = $"./Controllers";
-                    string controllerPath = $"{controllersDirPath}/{model}Controller.cs";
-                    try
+                    if (args[3] == "-nm" || args[3] == "--namespace")
                     {
-                        if (Directory.Exists(controllersDirPath))
-                        {
-                            Console.WriteLine(controllersDirPath);
-                        }
-                        else
-                        {
-                            DirectoryInfo di = Directory.CreateDirectory(controllersDirPath);
-                            Console.WriteLine("The directory was created successfully at {0}.",
-                                Directory.GetCreationTime(controllersDirPath));
-                        }
+                        Start();
                     }
-                    catch (Exception e)
+                    else
                     {
-                        Console.WriteLine(e);
-                        throw;
-                    }
-                    finally
-                    {
-                        try
-                        {
-                            FileStream fs = File.Create(controllerPath);
-                            fs.Close();
-                            var currDir = Directory.GetCurrentDirectory();
-                            FileInfo f = new FileInfo("BaseControllerTemplate.txt");
-                            var templatePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!,@"ControllerHandler/Templates/ControllerBaseTemplate.txt");
-                            Console.WriteLine(templatePath);
-                            string text = File.ReadAllText(templatePath);
-                            text = text.Replace("@NAMESPACE", $"{nmspace}");
-                            text = text.Replace("@MODEL", model);
-                            text = text.Replace("@CONTROLLER", $"{model}Controller");
-                            text = text.Replace("@MTL", $"{model.ToLower()}");
-                            File.WriteAllText(controllerPath,text);
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e);
-                            throw;
-                        }
+                        PrintError($"Error: Unexpected {args[3]} argument, expecting [-nm | --namespace].");
                     }
                 }
+                else
+                {
+                    PrintError($"Error: Unexpected {args[1]} argument, expecting [-m | --model].");
+                }
             }
+            else
+            {
+                PrintError("Error: 5 Arguments expected received " + this.args.Length);
+            }
+        }
+
+        private void Start()
+        {
+            string model = args[2];
+            string nmspace = args[4];
+            string controllerName = model + "Controller.cs";
+            string directoryName = @"Controllers";
+            bool directoryExists = Directory.Exists(directoryName);
+            bool modelExists = File.Exists(@"Models/" + model+".cs");
+            bool fileExists = File.Exists(@"Controllers/" + controllerName);
+
+            try
+            {
+                if (directoryExists)
+                {
+                    Console.WriteLine($"Creating {model} controller!");
+                }
+                else
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(directoryName);
+                    PrintMessage(
+                        $"The directory {directoryName} was created successfully at {Directory.GetCreationTime(directoryName)}.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            try
+            {
+                string controllerPath = Path.Combine(directoryName, controllerName);
+                
+                if (fileExists)
+                {
+                    PrintMessage($"Controller {model}Controller.cs was found, would you like to ovwerwrite it? Y/N");
+                    var answer = Console.ReadLine()?.ToLower();
+                    if (!answer.Contains("y"))
+                    {
+                        PrintMessage("Aborting controller creation.");
+                        return;
+                    }
+                }
+                if (!modelExists)
+                {
+                    PrintMessage($"Model {model} was not found, would you like to proceed? Y/N");
+                    var answer = Console.ReadLine()?.ToLower();
+                    if (!answer.Contains("y"))
+                    {
+                        PrintMessage("Aborting controller creation.");
+                        return;
+                    }
+
+                }
+
+
+                FileStream fs = File.Create(controllerPath);
+                fs.Close();
+                FileInfo f = new FileInfo("BaseControllerTemplate.txt");
+                var templatePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!,
+                    @"ControllerHandler/Templates/ControllerBaseTemplate.txt");
+                string text = File.ReadAllText(templatePath);
+                text = text.Replace("@NAMESPACE", $"{nmspace}");
+                text = text.Replace("@MODEL", model);
+                text = text.Replace("@CONTROLLER", $"{model}Controller");
+                text = text.Replace("@MTL", $"{model.ToLower()}");
+                File.WriteAllText(controllerPath, text);
+                PrintMessage($"Controller: {model}Controller.cs creation completed!"  );
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void PrintError(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public void PrintMessage(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
